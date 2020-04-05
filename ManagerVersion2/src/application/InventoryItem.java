e299675eac4f2384a2d394679c79d48cd4997275
Andrew Porth <porth@Andrews-MacBook-Pro.local>
2020-04-05 14:05:48 -0600

    Updated Inventory class, added InventoryReader and inventory_small.csv

--- ManagerVersion2/src/application/InventoryItem.java
+++ ManagerVersion2/src/application/InventoryItem.java
@@ -9,23 +9,78 @@ public class InventoryItem {
 	private String itemName;
 	private double calorie;
 	private double unitPrice;
-	private int amount;
-	private String supplyChannel;
+	private double onHand;
+	private String vendorName;
+	private double packSize;
+	private double packPrice;
+	private String itemID;
+	private double reorderPoint;
+	private String units;
+	
+	
 	
 	/**
 	 * constructor
 	 * @param name
 	 * @param calorie
-	 * @param price
-	 * @param amount
-	 * @param supplyChannel
+	 * @param packPrice
+	 * @param onHand
+	 * @param vendor
+	 * @param packSize
+	 * @param packPrice
+	 * @param itemID
 	 */
-	public InventoryItem(String name, double calorie, double price, int amount, String supplyChannel) {
+	public InventoryItem(String itemID, String name, double packSize, String units, String vendor, double price, double onHand, double calorie) {
 		itemName = name;
+		this.itemID = itemID;
 		this.calorie = calorie;
-		unitPrice = price;
-		this.amount = amount;
-		this.supplyChannel = supplyChannel;
+		packPrice = price;
+		this.onHand = onHand;
+		this.vendorName = vendor;
+		this.packSize = packSize;
+		this.units = units;
+		this.reorderPoint = packSize * .20;
+		this.unitPrice = price / packSize;
+	}
+
+	public double getPackSize() {
+		return packSize;
+	}
+
+	public void setPackSize(double packSize) {
+		this.packSize = packSize;
+	}
+
+	public double getPackPrice() {
+		return packPrice;
+	}
+
+	public void setPackPrice(double packPrice) {
+		this.packPrice = packPrice;
+	}
+
+	public String getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(String itemID) {
+		this.itemID = itemID;
+	}
+
+	public double getReorderPoint() {
+		return reorderPoint;
+	}
+
+	public void setReorderPoint(double reorderPoint) {
+		this.reorderPoint = reorderPoint;
+	}
+
+	public String getUnits() {
+		return units;
+	}
+
+	public void setUnits(String units) {
+		this.units = units;
 	}
 
 	public String getItemName() {
@@ -52,20 +107,20 @@ public class InventoryItem {
 		this.unitPrice = unitPrice;
 	}
 
-	public int getAmount() {
-		return amount;
+	public double getOnHand() {
+		return onHand;
 	}
 
-	public void setAmount(int amount) {
-		this.amount = amount;
+	public void setOnHand(double onHand) {
+		this.onHand = onHand;
 	}
 
-	public String getSupplyChannel() {
-		return supplyChannel;
+	public String getVendorName() {
+		return vendorName;
 	}
 
-	public void setSupplyChannel(String supplyChannel) {
-		this.supplyChannel = supplyChannel;
+	public void setVendorName(String vendorName) {
+		this.vendorName = vendorName;
 	}
 	
 	
--- ManagerVersion2/src/application/InventoryReader.java
+++ ManagerVersion2/src/application/InventoryReader.java
@@ -1,5 +1,70 @@
 package application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+//import java.util.Arrays;
+import java.util.Scanner;
+
 public class InventoryReader {
 
+	private ArrayList<InventoryItem> inventoryList;
+	
+
+	/**
+	 * Constructor takes filename of csv file as parameter
+	 * 
+	 * @param filename
+	 */
+	public InventoryReader(String filename) {
+		File file = new File(filename);
+		inventoryList = new ArrayList<InventoryItem>();
+
+		// read file
+		try {
+			Scanner scan = new Scanner(file);
+			scan.nextLine();
+			while (scan.hasNextLine()) {
+				String itemRow = scan.nextLine();
+				// System.out.println(itemRow);
+				String[] columnInfo = itemRow.split(",");
+				/*
+				 * column 0 for itemId (store as String) column 1 for name (store as
+				 * String) column 2 for packSize (store as Double) column 3 for units(store as
+				 * String) column 4 for vendor (store as String) column 5 for
+				 * packPrice (store as Double) column 6 for onHand (store
+				 * as Double) column 7 for calories
+				 * (store as Integer) 
+				 */
+				// write data to new InventoryItem object in ArrayList
+			
+					String itemId = columnInfo[0];
+					String name = columnInfo[1];
+					double packSize = Double.parseDouble(columnInfo[2]);
+					String units = columnInfo[3];
+					String vendor = columnInfo[4];
+					double price = Double.parseDouble(columnInfo[5]);
+					double onHand = Double.parseDouble(columnInfo[6]);
+					int calories = Integer.parseInt(columnInfo[7]);
+					InventoryItem item = new InventoryItem(itemId, name, packSize, units, vendor, price, onHand,
+							calories);
+					inventoryList.add(item);
+			}
+
+			scan.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * getter returns ArrayList of Flight objects
+	 * 
+	 * @return
+	 */
+	public ArrayList<InventoryItem> getInventoryList() {
+		return inventoryList;
+	}
 }
