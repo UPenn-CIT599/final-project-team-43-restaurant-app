beb9d66370942c48ccbea9773a20f0bbe5224d42
Yang Liu <liuy4@seas.upenn.edu>
2020-04-06 14:24:01 -0400

    renamed Orders.java to CustomerOrder.java

--- ManagerVersion2/src/application/CustomerOrder.java
+++ ManagerVersion2/src/application/CustomerOrder.java
@@ -1,8 +1,6 @@
 package application;
 
-
 import java.util.HashMap;
-
 
 /**
  * This class keeps track of the orders for the customers
@@ -11,12 +9,15 @@ import java.util.HashMap;
  *
  */
 
-public class Orders {
+public class CustomerOrder {
 
-
+	// an instance of the order class is associated with only one customer
 	private Customer customer;
 	private double totalPrice;
+
+	private boolean delivery;
 	private String estimatedTimeOfDelivery;
+
 
 	private HashMap<Taco, Integer> tacoItemToOrderQuantity;
 	private HashMap<Drink, Integer> drinkItemToOrderQuantity;
@@ -36,12 +37,13 @@ public class Orders {
 	 * @param customer the customer who is ordering
 	 * @param delivery set true if order is delivery; set false if order is pickup
 	 */
-	public Orders(Customer customer, boolean delivery) {
+	public CustomerOrder(Customer customer, boolean delivery) {
 
-		//order object contains the customer
+		// order object contains the customer and boolean value for delivery
 		this.setCustomer(customer);
+		this.setDelivery(delivery);
 
-		//order items for delivery are in a separate hashmap from pickup
+		// order items for delivery are in a separate hashmap from pickup
 		if (delivery == true) {
 			for (Taco taco : customer.getTacoOrders()) {
 				customerTacosToOrdersForDelivery.put(customer, taco);
@@ -69,11 +71,21 @@ public class Orders {
 	 * This method assigns quantity to each item that the customer ordered
 	 */
 	public void assignItemQuantity() {
-		
+
+	}
+
+	/**
+	 * This method calculates the total price of all orders for a given customer
+	 * 
+	 * @return the total value of all food items the customer ordered
+	 */
+	public double calculateTotalPrice() {
+
 	}
 
 	/**
 	 * Getter for customer
+	 * 
 	 * @return
 	 */
 	public Customer getCustomer() {
@@ -82,6 +94,7 @@ public class Orders {
 
 	/**
 	 * Setter for customer
+	 * 
 	 * @param customer
 	 */
 	public void setCustomer(Customer customer) {
@@ -90,6 +103,7 @@ public class Orders {
 
 	/**
 	 * Getter for total price
+	 * 
 	 * @return
 	 */
 	public double getTotalPrice() {
@@ -98,6 +112,7 @@ public class Orders {
 
 	/**
 	 * Setter for total price
+	 * 
 	 * @param totalPrice
 	 */
 	public void setTotalPrice(double totalPrice) {
@@ -106,6 +121,7 @@ public class Orders {
 
 	/**
 	 * Getter for estimated time of delivery
+	 * 
 	 * @return
 	 */
 	public String getEstimatedTimeOfDelivery() {
@@ -114,6 +130,7 @@ public class Orders {
 
 	/**
 	 * Setter for estimated time of delivery
+	 * 
 	 * @param estimatedTimeOfDelivery
 	 */
 	public void setEstimatedTimeOfDelivery(String estimatedTimeOfDelivery) {
@@ -122,6 +139,7 @@ public class Orders {
 
 	/**
 	 * Getter for the taco item to quantity hashmap
+	 * 
 	 * @return
 	 */
 	public HashMap<Taco, Integer> getTacoItemToOrderQuantity() {
@@ -130,6 +148,7 @@ public class Orders {
 
 	/**
 	 * Getter for the drink item to quantity hashmap
+	 * 
 	 * @return
 	 */
 	public HashMap<Drink, Integer> getDrinkItemToOrderQuantity() {
@@ -138,14 +157,30 @@ public class Orders {
 
 	/**
 	 * Getter for the side dish item to quantity hashmap
+	 * 
 	 * @return
 	 */
 	public HashMap<SideDish, Integer> getSideDishItemToOrderQuantity() {
 		return sideDishItemToOrderQuantity;
 	}
-	
-	
-	
-	
-	
+
+	/**
+	 * Getter for isDelivery
+	 * 
+	 * @return true for delivery; false for pickup
+	 */
+	public boolean isDelivery() {
+		return delivery;
+	}
+
+	/**
+	 * Setter for isDelivery
+	 * 
+	 * @param delivery true for delivery; false for pickup
+	 */
+	public void setDelivery(boolean delivery) {
+		this.delivery = delivery;
+	}
+
+
 }
