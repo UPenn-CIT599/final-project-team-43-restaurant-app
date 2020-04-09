8ff6486d21087981d5965f827d72b7f59012ab1a
yinjiez <yinjiez@seas.upenn.edu>
2020-04-09 15:43:46 -0500

    Add login function to manager 04092020

--- ManagerVersion2/src/application/CustomerHomePageController.java
+++ ManagerVersion2/src/application/CustomerHomePageController.java
@@ -10,7 +10,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
