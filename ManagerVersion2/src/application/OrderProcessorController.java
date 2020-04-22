25d76d628c63b7555d17f0edd6ec9e0b9e26ec9d
porth <porth@192.168.1.160>
2020-04-21 18:28:32 -0600

    Added methods to Inventory to purchase items and items needed list now sorts by vendorname. Satrted work on FXML for OrderProcessor

--- ManagerVersion2/src/application/OrderProcessorController.java
+++ ManagerVersion2/src/application/OrderProcessorController.java
@@ -1,7 +1,34 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class OrderProcessorController {
+	@FXML
+	private Button btn2;
+	
+	// Event Listener on Button[#btn2].onAction
+	@FXML
+	public void handleButtonAction(ActionEvent event) throws Exception {
+		Stage stage = (Stage) btn2.getScene().getWindow();
 
+		Parent root = FXMLLoader.load(getClass().getResource("OrderProcessor.fxml"));
+
+		if (event.getSource() == btn2) {
+			stage = (Stage) btn2.getScene().getWindow();
+			root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		}
+
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+
+	}
 }
