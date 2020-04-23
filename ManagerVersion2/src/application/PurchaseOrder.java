f294834e39ee72770b1c3b57550e8aef858a698d
porth <porth@192.168.1.160>
2020-04-23 09:13:57 -0600

    Updated methods in Order Processor Created Purchase Order Class.

---
+++ ManagerVersion2/src/application/PurchaseOrder.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PurchaseOrder {
+	HashMap<InventoryItem, Integer> items = new HashMap<InventoryItem, Integer>();
+	String vendor;
+	Double totalCost;
+	
+public PurchaseOrder(String vendorName){
+	this.items = items;
+	this.vendor = vendorName;
+	double itemCost;
+	
+			
+	
+}
+
+
+}
