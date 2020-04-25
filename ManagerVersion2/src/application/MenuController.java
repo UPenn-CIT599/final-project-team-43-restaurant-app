1093159ddf173bcb82153113b75abfb4fb4efc4a
Yang Liu <liuy4@seas.upenn.edu>
2020-04-25 15:36:01 -0400

    now displays calorie in menu

--- ManagerVersion2/src/application/MenuController.java
+++ ManagerVersion2/src/application/MenuController.java
@@ -19,15 +19,15 @@ import javafx.stage.Stage;
  */
 
 public class MenuController {
-	
-
 
 	@FXML
 	private Button btnBack1, btnBack2, btnBack3, btnOrder1, btnOrder2, btnOrder3;
 
 	@FXML
 	private TextField beefTacoPriceBox, chickenTacoPriceBox, veggieTacoPriceBox, drPepperPriceBox, laCroixPriceBox,
-			pepsiPriceBox, pacificoPriceBox, nachosPriceBox, tortillaPriceBox, riceAndBeansPriceBox;
+			pepsiPriceBox, pacificoPriceBox, nachosPriceBox, tortillaPriceBox, riceAndBeansPriceBox, beefTacoCalorieBox,
+			chickenTacoCalorieBox, veggieTacoCalorieBox, drPepperCalorieBox, laCroixCalorieBox, pepsiCalorieBox,
+			pacificoCalorieBox, nachosCalorieBox, tortillaCalorieBox, riceAndBeansCalorieBox;
 
 	@FXML
 	public void handleButtonAction(ActionEvent event) throws Exception {
@@ -73,6 +73,16 @@ public class MenuController {
 		nachosPriceBox = new TextField();
 		tortillaPriceBox = new TextField();
 		riceAndBeansPriceBox = new TextField();
+		beefTacoCalorieBox = new TextField();
+		chickenTacoCalorieBox = new TextField();
+		veggieTacoCalorieBox = new TextField();
+		drPepperCalorieBox = new TextField();
+		laCroixCalorieBox = new TextField();
+		pepsiCalorieBox = new TextField();
+		pacificoCalorieBox = new TextField();
+		nachosCalorieBox = new TextField();
+		tortillaCalorieBox = new TextField();
+		riceAndBeansCalorieBox = new TextField();
 	}
 
 	@FXML
@@ -84,20 +94,32 @@ public class MenuController {
 		Menu menu = new Menu();
 		menu.populateMenu("MenuList.csv", inv);
 
-
 		// Displays price of each food item in the menu GUI screen
 		beefTacoPriceBox.setText("$" + Double.toString(menu.tacos.get(0).getPrice()));
 		chickenTacoPriceBox.setText("$" + Double.toString(menu.tacos.get(1).getPrice()));
 		veggieTacoPriceBox.setText("$" + Double.toString(menu.tacos.get(2).getPrice()));
-		
+
 		drPepperPriceBox.setText("$" + Double.toString(menu.drinks.get(0).getPrice()));
 		laCroixPriceBox.setText("$" + Double.toString(menu.drinks.get(1).getPrice()));
 		pepsiPriceBox.setText("$" + Double.toString(menu.drinks.get(2).getPrice()));
 		pacificoPriceBox.setText("$" + Double.toString(menu.drinks.get(3).getPrice()));
-		
+
 		nachosPriceBox.setText("$" + Double.toString(menu.sides.get(0).getPrice()));
 		tortillaPriceBox.setText("$" + Double.toString(menu.sides.get(1).getPrice()));
 		riceAndBeansPriceBox.setText("$" + Double.toString(menu.sides.get(2).getPrice()));
+		
+		beefTacoCalorieBox.setText(Integer.toString(menu.tacos.get(0).getCalories()) + " calories");
+		chickenTacoCalorieBox.setText(Integer.toString(menu.tacos.get(1).getCalories()) + " calories");
+		veggieTacoCalorieBox.setText(Integer.toString(menu.tacos.get(2).getCalories()) + " calories");
+		
+		drPepperCalorieBox.setText(Integer.toString(menu.drinks.get(0).getCalories()) + " calories");
+		laCroixCalorieBox.setText(Integer.toString(menu.drinks.get(1).getCalories()) + " calories");
+		pepsiCalorieBox.setText(Integer.toString(menu.drinks.get(2).getCalories()) + " calories");
+		pacificoCalorieBox.setText(Integer.toString(menu.drinks.get(3).getCalories()) + " calories"); 
+		
+		nachosCalorieBox.setText(Integer.toString(menu.sides.get(0).getCalories()) + " calories"); 
+		tortillaCalorieBox.setText(Integer.toString(menu.sides.get(1).getCalories()) + " calories");
+		riceAndBeansCalorieBox.setText(Integer.toString(menu.sides.get(2).getCalories()) + " calories");
 
 	}
 }
